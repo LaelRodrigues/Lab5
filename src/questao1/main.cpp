62eff5734417b44247fffcc407cdf34bd27228b0
LaelRodrigues <laelrodrigues7@gmail.com>
2017-11-05 21:38:10 -0300

    Adicionando o arquivo main.cpp da questão 1

---
+++ src/questao1/main.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file	main1.cpp
+ * @brief	Codigo fonte de teste da funcao closest2mean
+ *			presente no arquivo intervalo.h
+ * @author	LaelRodrigues (laelRodrigues7@gmail.com)
+ * @since	05/11/2017
+ * @date	05/11/2017
+ */
+
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include <vector>
+using std::vector;
+
+#include "intervalo.h"
+
+/** @brief Funcao principal */
+int main() {
+	vector<float> v { 1, 2, 3, 30, 40, 50 };
+	auto result = closest2mean(v.begin(), v.end());
+	cout << "O valor do elemento mais proximo da media do intervalo e: " << (*result) << endl;
+	return 0;
+}
